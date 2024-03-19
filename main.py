b26ad55fdfaf0a341c61dca924eb67304164327a
Lorran Caetano Machado Lopes <lorrancml@al.insper.edu.br>
2024-03-19 14:37:37 -0300

    tenta fazer roteiro 5

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
 import sys
 import re
 
+# Palavras reservadas
+RESERVED_KEYWORDS = ['print']
+
+
 # 1. Implementar a árvore sintática abstrata (AST)
 class PrePro:
     @staticmethod
     def filter(code):
-        # Remove comentários
-        return re.sub(r"--.*", "", code)
+        # Remove comentários e linhas em branco 
+        code = re.sub(r'//.*', '', code)
+        code = re.sub(r'\n\s*\n', '\n', code)
+        return code
+
+
+class SymbolTable:
+    def __init__(self):
+        self.symbol_table = {}
+
+    def set(self, identifier, value):
+        self.symbol_table[identifier] = value
+
+    def get(self, identifier):
+        if identifier in self.symbol_table:
+            return self.symbol_table[identifier]
+        else:
+            raise NameError(f"Variável '{identifier}' não definida na tabela de símbolos.")
 
 
 class Node:
@@ -18,27 +38,76 @@ class Node:
         pass
 
 
+class Block(Node):
+    def evaluate(self, symbol_table):
+        for child in self.children:
+            child.evaluate(symbol_table)
+
+
+class Assignment(Node):
+    def evaluate(self, symbol_table):
+        identifier = self.children[0]
+        value_node = self.children[1]  # Acessando o nó de valor
+
+        if isinstance(value_node, IntVal):  # Verificando se o nó de valor é do tipo IntVal
+            value = value_node.evaluate()  # Se for, apenas obtemos o valor
+        else:
+            value = value_node.evaluate(symbol_table)  # Caso contrário, avaliamos a expressão
+
+        symbol_table.set(identifier, value)
+
+
 class BinOp(Node):
-    def evaluate(self):
-        left = self.children[0].evaluate()
-        right = self.children[1].evaluate()
-        if self.value == '+':
-            return left + right
-        elif self.value == '-':
-            return left - right
-        elif self.value == '*':
-            return left * right
-        elif self.value == '/':
-            return left // right
+    def evaluate(self, symbol_table):
+        left = self.children[0]
+        right = self.children[1]
+
+        if not isinstance(left, IntVal):
+            left = left.evaluate(symbol_table)
+
+        if not isinstance(right, IntVal):
+            right = right.evaluate(symbol_table)
+
+        if isinstance(left, IntVal) and isinstance(right, IntVal):
+            if self.value == '+':
+                return left.value + right.value
+            elif self.value == '-':
+                return left.value - right.value
+            elif self.value == '*':
+                return left.value * right.value
+            elif self.value == '/':
+                return left.value // right.value
+            else:
+                raise TypeError("Operação não suportada para operandos não inteiros")
+        # Se pelo menos uma das partes for um inteiro, tratamos esse caso separadamente
+        elif isinstance(left, int) and isinstance(right, IntVal):
+            if self.value == '+':
+                return left + right.value
+            elif self.value == '-':
+                return left - right.value
+            elif self.value == '*':
+                return left * right.value
+            elif self.value == '/':
+                return left // right.value
+        elif isinstance(left, IntVal) and isinstance(right, int):
+            if self.value == '+':
+                return left.value + right
+            elif self.value == '-':
+                return left.value - right
+            elif self.value == '*':
+                return left.value * right
+            elif self.value == '/':
+                return left.value // right
+        else:
+            raise TypeError("Operação não suportada para operandos não inteiros")
 
 
 class UnOp(Node):
-    def evaluate(self):
+    def evaluate(self, symbol_table):
         if self.value == '-':
-            return -self.children[0].evaluate()
+            return -self.children[0].evaluate(symbol_table)
         else:
-            return self.children[0].evaluate()
-
+            return self.children[0].evaluate(symbol_table)
 
 
 class IntVal(Node):
@@ -46,9 +115,19 @@ class IntVal(Node):
         return self.value
 
 
+
 class NoOp(Node):
     def evaluate(self):
         pass
+
+
+class Print(Node):
+    def evaluate(self, symbol_table):
+        #avalia se children[0] é um IntVal ou uma expressão
+        if isinstance(self.children[0], IntVal):
+            print(self.children[0].evaluate())
+        else:
+            print(self.children[0].evaluate(symbol_table))
 
 
 class Token:
@@ -84,6 +163,10 @@ class Tokenizer:
     def get_next_token(self):
         while self.current_char is not None:
 
+            if self.current_char == '\n':
+                self.advance()
+                return Token('NEWLINE', '\n')
+
             if self.current_char.isspace():
                 self.skip_whitespace()
                 continue
@@ -115,6 +198,23 @@ class Tokenizer:
                 self.advance()
                 return Token('RPAR', ')')
 
+            if self.current_char == '=':
+                self.advance()
+                return Token('ASSIGN', '=')
+
+            # Verificar o caso de ser um identificador de variável (começa com letra e contém letras e números)
+            if self.current_char.isalpha():
+                identifier = ''
+                while self.current_char.isalnum() or self.current_char == "_":
+                    identifier += self.current_char
+                    self.advance()
+
+                if identifier in RESERVED_KEYWORDS:
+                    # Se for um print, desvia
+                    if identifier == 'print':
+                        return Token('PRINT', 'print')
+                return Token('IDENTIFIER', identifier)
+
             # Se não corresponder a nenhum dos tipos de token conhecidos, levanta um erro
             raise SyntaxError("Caractere inválido encontrado: {}".format(self.current_char))
 
@@ -129,6 +229,57 @@ class Tokenizer:
 class Parser:
     def __init__(self, tokenizer: Tokenizer):
         self.tokenizer = tokenizer
+        self.symbol_table = SymbolTable
+
+    @staticmethod
+    def parseBlock():  # resolve cada linha, que é um Statement
+        token = Parser.tokenizer.selectNext()
+        # cria o nó de bloco
+        block_node = Block()
+        # tabela de símbolos
+        Parser.symbol_table = SymbolTable()
+        while token.type != 'EOF':
+            statement = Parser.parseStatement(token)
+            block_node.children.append(statement)
+            token = Parser.tokenizer.selectNext()
+        # fazer o evaluate do bloco
+        block_node.evaluate(Parser.symbol_table)
+
+    @staticmethod
+    def parseStatement(token):
+        # Se o token for um /n, não faz nada
+        if token.type == 'NEWLINE':
+            return
+        # Se o token for um identificador, cria um nó de atribuição (Assignment)
+        elif token.type == 'IDENTIFIER':
+            identifier = token.value
+            token = Parser.tokenizer.selectNext()
+            if token.type == 'ASSIGN':
+                expression, _ = Parser.parseExpression()
+                assignment_node = Assignment()
+                assignment_node.value = token.value
+                assignment_node.children.append(identifier)
+                assignment_node.children.append(expression)
+                Parser.symbol_table.set(identifier, expression)
+                return assignment_node
+
+        # Se o token for um print, verifica se tem um (, expressao e um ). Se sim, cria um nó de print
+        elif token.type == 'PRINT':
+            token = Parser.tokenizer.selectNext()
+            if token.type == 'LPAR':
+                expression, token = Parser.parseExpression()
+                #token = Parser.tokenizer.selectNext() #testando comentar
+                if token.type == 'RPAR':
+                    print_node = Print()
+                    print_node.value = "print"
+                    print_node.children.append(expression)
+                    return print_node
+                else:
+                    raise SyntaxError("Erro: Parênteses não fechados")
+            else:
+                raise SyntaxError("Erro: Parênteses não abertos")
+        else:
+            raise SyntaxError("Erro: Token inesperado")
 
     @staticmethod
     def parseExpression():
@@ -140,7 +291,7 @@ class Parser:
             bin_op_node.children.append(result_expression)
             bin_op_node.children.append(result_term)
             result_expression = bin_op_node
-        return result_expression
+        return result_expression, token
 
     @staticmethod
     def parseTerm():
@@ -159,8 +310,15 @@ class Parser:
         token = Parser.tokenizer.selectNext()
         if token.type == 'INT':
             return IntVal(token.value), Parser.tokenizer.selectNext()
+        # Se for um identificador, verifica se está na tabela de símbolos
+        elif token.type == 'IDENTIFIER':
+            identifier = token.value
+            if Parser.symbol_table.get(identifier):
+                return Parser.symbol_table.get(identifier), Parser.tokenizer.selectNext()
+            else:
+                raise NameError(f"Variável '{identifier}' não definida na tabela de símbolos.")
         elif token.type == 'LPAR':
-            result_expression = Parser.parseExpression()
+            result_expression, _ = Parser.parseExpression()
             if Parser.tokenizer.next.type == 'RPAR':
                 return result_expression, Parser.tokenizer.selectNext()
             else:
@@ -176,10 +334,11 @@ class Parser:
     @staticmethod
     def run(code):
         Parser.tokenizer = Tokenizer(code)
-        result = Parser.parseExpression()
-        if Parser.tokenizer.next.type != 'EOF':
-            raise SyntaxError("Erro: Tokens inesperados no final da expressão")
-        return result
+        # chama o parser block para criar a árvore e salva para retornar no final a árvore a ser usada no evaluate
+        Parser.parseBlock()
+        # if Parser.tokenizer.next.type != 'EOF':
+        #     raise SyntaxError("Erro: Tokens inesperados no final da expressão")
+
 
 
 def main():
@@ -188,9 +347,9 @@ def main():
     filename = sys.argv[1]
     with open(filename, 'r') as file:
         code = file.read()
-    result = Parser.run(code)
-    print(result.evaluate())
-
+#     code = '''x = 3
+# print(x)'''
+    Parser.run(code)
 
 if __name__ == "__main__":
     main()
